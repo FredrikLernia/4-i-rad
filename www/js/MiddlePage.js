6b9a113e20d2657ddc779e0b68600abbb1926aeb
Fredrik Sunding <fredrik.sunding@gmail.com>
2018-12-15 22:18:04 +0100

    Temporary routing, properties and image

--- www/js/MiddlePage.js
+++ www/js/MiddlePage.js
@@ -2,6 +2,11 @@ class MiddlePage extends Component{
 
   constructor(){
     super();
-    
+    this.addRoute('/resultat', 'Resultat');
+    this.result = 'won';
+    this.name = 'Fredrik';
+    this.moves = 14;
+    this.time = 15.63;
+    this.isHighscore = true;
   }
 }
