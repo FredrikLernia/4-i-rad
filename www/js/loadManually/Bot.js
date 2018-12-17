194e22ba202c8018d399cde74da1ce642b5ffab9
Oliver Nilsson <oliver.nilsson94@gmail.com>
2018-12-17 15:46:37 +0100

    Added feature for giving bots unique name
    bots cant have same name aswell

--- www/js/loadManually/Bot.js
+++ www/js/loadManually/Bot.js
@@ -2,11 +2,19 @@ class Bot extends Player{
 
   constructor(color){
     super(color);
-    this.name = 'Bot';
+    this.namesArray = ['Klas', 'Hans', 'Olof', 'David', 'Per', 'Herman'];
+    this.name = this.getRandomName();
   }
 
   getRandomNumber() {
     let rand = Math.floor(Math.random() * 7);
     return rand;
+  }
+
+  getRandomName(){
+    console.log("arraylist ", this.namesArray.length-1)
+    let rand = Math.round(Math.random() * (this.namesArray.length-1));
+    console.log(rand);
+    return this.namesArray[rand];
   }
 }
