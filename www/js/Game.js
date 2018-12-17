81d8aa4ea4da4282283347104b22a773a83720c3
joel <joel.edling1@gmail.com>
2018-12-17 15:51:13 +0100

    added visuals for winning horisontal and vertical..diags in progress

--- www/js/Game.js
+++ www/js/Game.js
@@ -69,8 +69,9 @@ class Game extends Component {
 
     if (this.winChecker(this.playerTurn.color)) {
       this.gameOver = true;
+      // this.savedGame = JSON.stringify(this.columns);
       setTimeout(() => {
-        this.restartGame();
+        //this.restartGame();
         // Go to result page with either win or lose from here
       }, 2000)
     }
@@ -96,24 +97,47 @@ class Game extends Component {
       for (let row = 0; row < 6; row++) {
         let hor = true, ver = true, dia1 = true, dia2 = true;
         let horCheck = false, verCheck = false, dia1Check = false, dia2Check = false;
-
+        let vertArr = [];
+        let horArr = [];
+        let dia1Arr = [];
+        let dia2Arr = [];
         for (let i = 0; i < 4; i++) {
 
           horCheck = this.columns[col + i];
           hor = hor && horCheck && this.columns[col + i].slots[row].brickInside[0] !== undefined && this.columns[col + i].slots[row].brickInside[0].color === playerColor;
+          hor && (horArr.push(this.columns[col + i].slots[row]));
 
           verCheck = this.columns[col].slots[row + i];
           ver = ver && verCheck && this.columns[col].slots[row + i].brickInside[0] !== undefined && this.columns[col].slots[row + i].brickInside[0].color === playerColor;
+          ver && (vertArr.push(this.columns[col].slots[row + i]));
 
           dia1Check = this.columns[col + i];
           dia1 = dia1 && dia1Check && this.columns[col + i].slots[row + i] !== undefined && this.columns[col + i].slots[row + i].brickInside[0] !== undefined && this.columns[col + i].slots[row + i].brickInside[0].color === playerColor;
+          dia1 && (dia1Arr.push(this.columns[col + i].slots[row + i]));
 
           dia2Check = this.columns[col + i];
           dia2 = dia2 && dia2Check && this.columns[col + i].slots[row - i] !== undefined && this.columns[col + i].slots[row - i].brickInside[0] !== undefined && this.columns[col + i].slots[row - i].brickInside[0].color === playerColor;
-
+          dia2 && (dia2Arr.push(this.columns[col + i].slots[row - i]))
         }
 
         if (hor || ver || dia1 || dia2) {
+          if(vertArr.length === 4){
+            this.winSlots = vertArr;
+          }
+          else if(horArr.length === 4){
+            this.winSlots = horArr;
+          }
+          else if(dia1Arr === 4){
+            this.winSlots = dia1Arr;
+          }
+          else if(dia2Arr === 4) {
+            this.winSlots = dia2Arr;
+          }
+          for(let slot of this.winSlots){
+            slot.win = true;
+            slot.render();
+          }
+          console.log(this.winSlots);
           return true;
         }
       }
