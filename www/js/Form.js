60f3c9e35c86af7fba00f7772a1e7e6eb9dafbdd
sunsneezer <xil_wong@hotmail.com>
2018-12-19 15:51:11 +0100

    player: name-, color- and type  validator

--- www/js/Form.js
+++ www/js/Form.js
@@ -12,17 +12,68 @@ class Form extends Component {
     ];
     this.playerOne = {};
     this.playerTwo = {};
-  }
+  } 
 
   storeNames() {
     // Saving the name inputs to the player objects
     this.playerOne.name = this.baseEl.find('#player-1').find('.player-name').val();
     this.playerTwo.name = this.baseEl.find('#player-2').find('.player-name').val();
+    
+    if(this.validatePlayerNames() && this.validatePlayerType() && this.validatePlayerColor()){
+    
+      this.createPlayer(this.playerOne);
+      this.createPlayer(this.playerTwo);
+    
+      this.gamePage.createGame();
+    }
+  }
+  
+  validatePlayerNames(){
 
-    this.createPlayer(this.playerOne);
-    this.createPlayer(this.playerTwo);
+    if (this.playerOne.name.length < 2 || this.playerOne.name.length > 9){
+      this.baseEl.find('#player-1-text').show();
+    }
+    else {
+      this.baseEl.find('#player-1-text').hide();
+    }
+    if (this.playerTwo.name.length < 2 || this.playerTwo.name.length > 9){
+      this.baseEl.find('#player-2-text').show();
+    }
+    else {
+      this.baseEl.find('#player-2-text').hide();
+    }
+    if((this.playerOne.name.length > 1 && this.playerOne.name.length < 11) && (this.playerTwo.name.length > 1 && this.playerTwo.name.length < 11)){
+      return true;
+    }
+    return false;
+  }
 
-    this.gamePage.createGame();
+  validatePlayerType(){
+    let currentPlayerType;
+    for (let i = 1; i <= 2; i++) {
+      currentPlayerType = this.baseEl.find('#player-' + i).find('.choose-player-type')[0].innerText;
+      if (currentPlayerType === '👨‍💻 Spelartyp ') {
+        this.baseEl.find('#player-' + i).find('#player-type').show();
+      } 
+    }  
+    if (this.baseEl.find('#player-1').find('.choose-player-type')[0].innerText != '👨‍💻 Spelartyp ' && this.baseEl.find('#player-2').find('.choose-player-type')[0].innerText != '👨‍💻 Spelartyp ') {
+      return true;
+    } 
+    return false;  
+  }
+
+  validatePlayerColor(){
+    let currentPlayerColor;
+    for(let i = 1; i <= 2; i++) {
+      currentPlayerColor = this.baseEl.find('#player-' + i).find('.choose-color')[0].innerText;
+      if (currentPlayerColor === '🎨 Färg '){
+        this.baseEl.find('#player-' + i).find('#player-color').show();
+      }
+    }
+    if (this.baseEl.find('#player-1').find('.choose-color')[0].innerText != '🎨 Färg ' && this.baseEl.find('#player-2').find('.choose-color')[0].innerText!= '🎨 Färg '){
+      return true;
+    }
+    return false;
   }
 
   /* createPlayer(player) {
